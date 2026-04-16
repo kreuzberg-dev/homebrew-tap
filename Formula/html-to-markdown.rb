3975ea39b6bd56a6cd4065b9f95363a154cd1103
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-04-16 10:10:04 +0200

    fix: update html-to-markdown v3.2.0 tarball sha256

--- Formula/html-to-markdown.rb
+++ Formula/html-to-markdown.rb
@@ -2,7 +2,7 @@ class HtmlToMarkdown < Formula
   desc "High-performance HTML to Markdown converter powered by Rust"
   homepage "https://github.com/kreuzberg-dev/html-to-markdown"
   url "https://github.com/kreuzberg-dev/html-to-markdown/archive/v3.2.0.tar.gz"
-  sha256 "e33e1df3bd692a7ddc0476cbe92c52124bf4b414b3ee901a1ce9233e0cbbb2f7"
+  sha256 "31c8c38e6546ce75d7391b9d05338d41ca812dfc3d0376dcb30e7484dd7c5f72"
   license "MIT"
 
   depends_on "rust" => :build
